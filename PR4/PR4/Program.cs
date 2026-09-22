c52ce4cf3d073a9465ed0ceb4ff1faeb7c13ad6a
spece <senkobogdan4@gmail.com>
2026-09-22 23:01:43 +0500

    Add files via upload

---
+++ PR4/PR4/Program.cs
@@ -0,0 +1,29 @@
+﻿string offer = Console.ReadLine();
+char[] offer_split = offer.ToCharArray();
+
+for (int i = 0; i < offer_split.Length; i+= 2)
+{
+    offer_split[i] = char.ToUpper(offer_split[i]);
+}
+
+offer = new string(offer_split);
+Console.WriteLine(offer);
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= number; i++)
+{
+    Console.WriteLine(i);
+}
+
+int[] arr_5 = new int[5];
+
+for (int i = 0; i < arr_5.Length; i++)
+{
+    arr_5[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = arr_5.Length - 1; i >= 0; i--)
+{
+    Console.Write(arr_5[i] + " ");
+}
